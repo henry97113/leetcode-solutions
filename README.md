a0f0426332166538503f34a8a665f3b504446d5a
Henry Chou <38032437+henry97113@users.noreply.github.com>
2022-08-31 02:03:49 +0800

    add the answer of problem 14

--- README.md
+++ README.md
@@ -15,3 +15,4 @@
 11. [Two Sum II - Input Array Is Sorted](https://github.com/henry97113/neetcode-io-solutions/blob/main/neetcode-150/11_two_sum_ii.ts)
 12. [3 Sum](https://github.com/henry97113/neetcode-io-solutions/blob/main/neetcode-150/12_3_sum.ts)
 13. [Container with Most Water](https://github.com/henry97113/neetcode-io-solutions/blob/main/13_container_with_most_water.ts)
+14. [Trapping Rain Water](https://github.com/henry97113/neetcode-io-solutions/blob/main/14_trapping_rain_water.ts)
---
+++ neetcode-150/14_trapping_rain_water.ts
@@ -0,0 +1,72 @@
+/**
+ * https://leetcode.com/problems/trapping-rain-water/
+ * Time: O(n) | Space: O(1)
+ */
+
+/**
+ * Time: O(n) | Space: O(n)
+ */
+function trap1(height: number[]): number {
+  const maxL = Array.from({ length: height.length }, () => 0);
+  const maxR = Array.from({ length: height.length }, () => 0);
+
+  for (let i = 1; i < height.length; i++) {
+    if (height[i - 1] > maxL[i - 1]) {
+      maxL[i] = height[i - 1];
+    } else {
+      maxL[i] = maxL[i - 1];
+    }
+  }
+
+  for (let i = height.length - 2; i >= 0; i--) {
+    if (height[i + 1] > maxR[i + 1]) {
+      maxR[i] = height[i + 1];
+    } else {
+      maxR[i] = maxR[i + 1];
+    }
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    const water = Math.max(Math.min(maxL[i], maxR[i]) - height[i], 0);
+    sum += water;
+  }
+
+  return sum;
+}
+
+/**
+ * Time: O(n) | Space: O(1)
+ */
+function trap2(height: number[]): number {
+  let sum = 0;
+  let maxL = height[0];
+  let maxR = height[height.length - 1];
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    if (maxL <= maxR) {
+      left++;
+
+      const water = Math.max(Math.min(maxL, maxR) - height[left], 0);
+      sum += water;
+
+      if (height[left] > maxL) {
+        maxL = height[left];
+      }
+    } else {
+      right--;
+
+      const water = Math.max(Math.min(maxL, maxR) - height[right], 0);
+      sum += water;
+
+      if (height[right] > maxR) {
+        maxR = height[right];
+      }
+    }
+  }
+
+  return sum;
+}
