d59376f1244c83e0165361cf00da284b97b7fa6e
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-06-18 17:27:01 +0800

    add problem 287 solution

---
+++ neetcode-150/287_find_the_duplicate_number/solution.ts
@@ -0,0 +1,16 @@
+function findDuplicate(nums: number[]): number {
+  let [slow, fast] = [0, 0]
+
+  do {
+    slow = nums[slow]
+    fast = nums[nums[fast]]
+  } while (slow !== fast)
+
+  let slow2 = 0
+  while (slow !== slow2) {
+    slow = nums[slow]
+    slow2 = nums[slow2]
+  }
+
+  return slow
+}
