bdcc10d44485ab7052a06b9b3a209720bf5d4ece
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-07-19 23:42:50 +0800

    add problem 215 solution 2

--- 215_kth_largest_element_in_an_array/solution.ts
+++ 215_kth_largest_element_in_an_array/solution.ts
@@ -1,3 +1,4 @@
+// Max Heap
 function findKthLargest(nums: number[], k: number): number {
   const mpq = new MaxPriorityQueue()
 
@@ -11,4 +12,29 @@ function findKthLargest(nums: number[], k: number): number {
   }
 
   return mpq.front().element
+}
+
+// Quick Select
+function findKthLargest(nums: number[], k: number): number {
+  k = nums.length - k
+
+  function quickSelect(l: number, r: number) {
+    const pivot = nums[r]
+    let p = l
+
+    for (let i = l; i < r; i++) {
+      if (nums[i] <= pivot) {
+        ;[nums[i], nums[p]] = [nums[p], nums[i]]
+        p++
+      }
+    }
+
+    ;[nums[p], nums[r]] = [nums[r], nums[p]]
+
+    if (k < p) return quickSelect(l, p - 1)
+    else if (k > p) return quickSelect(p + 1, r)
+    else return nums[p]
+  }
+
+  return quickSelect(0, nums.length - 1)
 }
