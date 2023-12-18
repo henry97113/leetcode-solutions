ad8028028393611a497b4edbfcc096cee0815447
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-12-18 17:49:25 +0800

    add problem 876 solution

---
+++ 876_middle_of_the_linked_list/solution.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        slow = fast = head
+
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
