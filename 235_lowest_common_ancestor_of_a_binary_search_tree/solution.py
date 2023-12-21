9e6bc2150f3d9358e2421e02240c79052db8df7e
Henry Chou <38032437+henry97113@users.noreply.github.com>
2023-12-21 15:32:29 +0800

    add problem 235 solution

---
+++ 235_lowest_common_ancestor_of_a_binary_search_tree/solution.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        curr = root
+
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else:
+                break
+
+        # We know the curr is a TreeNode, but need to correct the typing error.
+        if curr is None:
+            raise ValueError("The lowest common ancestor was not found in the tree.")
+
+        return curr
